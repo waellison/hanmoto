faea7cfc95d7bcca469806c0096767b5fb35ed1b
William Ellison <waellison@me.com>
2021-10-20 20:44:36 -0400

    Added db object to the model package.

---
+++ app/models/__init__.py
@@ -0,0 +1,18 @@
+"""
+Model-specific logic for WillPress.
+
+"An Excellent Blog Engine"
+
+Copyright (c) 2021 by William Ellison.  This program is licensed under
+the terms of the Do What the Fuck You Want To Public License, version 2
+or later, as described in the COPYING file at the root of this
+distribution.
+
+William Ellison
+<waellison@gmail.com>
+October 2021
+"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
