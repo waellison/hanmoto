4eef9706d2dcc9313d911517fc1187039660527c
William Ellison <waellison@me.com>
2021-10-24 14:31:41 -0400

    Added homepage blueprint to app factory.

--- app/__init__.py
+++ app/__init__.py
@@ -13,35 +13,10 @@ William Ellison
 October 2021
 """
 import os
-from datetime import datetime
 from flask import Flask
 from flask_migrate import Migrate
-from . import models
-from . import views
-
-
-def wep_ap_date_format(date: datetime) -> str:
-    abbreviations = {
-        "months": [
-            "Jan.",
-            "Feb.",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "Aug.",
-            "Sept.",
-            "Oct.",
-            "Nov.",
-            "Dec."
-        ]
-    }
-    day = date.day
-    month = abbreviations["months"][date.month - 1]
-    year = date.year
-    return f"{month} {day}, {year}"
-
+from .models import db
+from .views import categories, posts, home
 
 
 def wep_create_app(test_config=None):
@@ -65,14 +40,13 @@ def wep_create_app(test_config=None):
     except OSError:
         pass
 
-    from .models import db
     db.app = app
     db.init_app(app)
     db.create_all()
     _ = Migrate(app, db)
 
-    from .views import posts, categories
     app.register_blueprint(posts.bp)
     app.register_blueprint(categories.bp)
+    app.register_blueprint(home.bp)
 
     return app
