902c74a6d4df8062e895611a25c40f176d2aa969
William Ellison <waellison@me.com>
2021-10-26 16:42:09 -0400

    Corrected response code in logout from 307 to 303

--- app/views/logout.py
+++ app/views/logout.py
@@ -6,4 +6,4 @@ bp = Blueprint("logout", __name__, url_prefix='/logout')
 @bp.route('', methods=['POST'])
 def logout_active_user():
     session.pop('user', default=None)
-    return redirect(url_for('home.show_homepage'), code=307)
+    return redirect(url_for('home.show_homepage'), code=303)
