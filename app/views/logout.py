a10fe4e8abd6c9656bcdac920e5049db9900fca9
William Ellison <waellison@me.com>
2021-10-26 16:33:12 -0400

    Removed unused import from views.logout.
    Added a 303 to response in logout_active_user.

--- app/views/logout.py
+++ app/views/logout.py
@@ -1,4 +1,3 @@
-import os
 from flask import Blueprint, session, url_for, redirect
 
 bp = Blueprint("logout", __name__, url_prefix='/logout')
@@ -7,4 +6,4 @@ bp = Blueprint("logout", __name__, url_prefix='/logout')
 @bp.route('', methods=['POST'])
 def logout_active_user():
     session.pop('user', default=None)
-    return redirect(url_for('home.show_homepage'))
+    return redirect(url_for('home.show_homepage'), code=307)
