46861aa1e0ec959ccc12481e21cc1e783dc9faf1
William Ellison <waellison@me.com>
2021-11-06 09:03:24 -0400

    Added database init script

---
+++ data/flask_init.sql
@@ -0,0 +1,17 @@
+-- init for the WillPress database.
+
+drop database if exists willpress;
+
+create database willpress;
+
+\c willpress
+
+set statement_timeout = 0;
+set lock_timeout = 0;
+set client_encoding = 'UTF8';
+set standard_conforming_strings = on;
+set check_function_bodies = false;
+set client_min_messages = warning;
+set default_tablespace = '';
+set default_with_oids = false;
+
