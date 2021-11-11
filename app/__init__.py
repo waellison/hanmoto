d10b056b38755dc14b1f789f3999179e85740eea
William Ellison <waellison@me.com>
2021-11-10 19:57:01 -0500

    Added a putative attempt at a Dockerfile.
    This does not work completely as yet: the database is inaccessible from
    the container holding the actual files for Uchapishaji.

--- app/__init__.py
+++ app/__init__.py
@@ -25,17 +25,17 @@ def wep_create_app(test_config=None):
     dotenv.load_dotenv("..")
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=os.getenv('secret_key'),
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/willpress',
+        SECRET_KEY=os.getenv("secret_key"),
+        SQLALCHEMY_DATABASE_URI="postgresql://postgres@pg:5432/willpress",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_ECHO=True,
         SESSION_PERMANENT=False,
         SESSION_TYPE="filesystem",
-        FLASK_ADMIN_SWATCH='cerulean',
+        FLASK_ADMIN_SWATCH="cerulean",
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.mapping(test_config)
 
