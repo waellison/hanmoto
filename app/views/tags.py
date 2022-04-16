6d63862e9cffcffb8d546c0506e378fc94ae4299
William Ellison <waellison@gmail.com>
2022-04-15 20:19:46 -0400

    Resolved several issues via Pylint.

--- app/views/tags.py
+++ app/views/tags.py
@@ -10,12 +10,10 @@ distribution.
 William Ellison
 <waellison@gmail.com>
 """
-from datetime import datetime
-from sqlalchemy import sql
 from sqlalchemy.exc import SQLAlchemyError
-from flask import Blueprint, Response, jsonify, request, redirect, url_for, abort
-from ..models import db
-from ..models.WEPTag import WEPTag
+from flask import Blueprint, Response, jsonify, request, abort
+from app.models import db
+from app.models.WEPTag import WEPTag
 
 
 bp = Blueprint("tags-view", __name__, url_prefix="/tags")
@@ -23,45 +21,64 @@ bp = Blueprint("tags-view", __name__, url_prefix="/tags")
 
 @bp.route("/<slug>", methods=["GET"])
 def read_specific_tag_by_slug(slug: str) -> Response:
+    """
+    Retrieve a specific tag by its slug.
+
+    Args:
+        slug [string]: the slug to try retrieving
+
+    Returns:
+        A JSON object representing the found tag, or a
+        404 error otherwise.
+    """
     tag_id = WEPTag.query.filter_by(slug=slug)[0].id
     return read_specific_tag_json(tag_id)
 
 
 @bp.route("/<int:gag_id>", methods=["GET"])
 def read_specific_tag_json(tag_id: int) -> Response:
+    """
+    Retrieve a specific slug by its numeric id.
+
+    Args:
+        tag_id [int]: the tag ID to retreive
+
+    Returns:
+        A JSON object representing the found tag, or a
+        404 error otherwise.
+    """
     tag = WEPTag.query.get_or_404(tag_id)
     return jsonify(tag.json_serialize())
 
 
 @bp.route("/all", methods=["GET"])
 def list_all_tags():
-    results = db.session.execute(
-        sql.text(
-            """
-        WITH tag_occurrences AS (
-            SELECT tag_id, COUNT(*) AS posts_with_tag
-            FROM post_tags
-            GROUP BY tag_id
-        ) SELECT t.id, t.name, tao.posts_with_tag
-        FROM tag_occurrences tao
-        INNER JOIN tags t
-        ON t.id = tao.tag_id
-        ORDER BY posts_with_tag DESC;
     """
-        )
-    ).fetchall()
+    Retrieve all tags from within WillPress.
 
+    Returns:
+        A JSON object keyed on tag ID with information about every
+        tag within WillPress.
+    """
     tags = WEPTag.query.order_by(WEPTag.id)
 
     return jsonify({tag.id: tag.json_serialize() for tag in tags})
 
 
 @bp.route("/new", methods=["POST"])
-def add_new_category():
-    cat = WEPCategory(request.form["name"], request.form["summary"], None)
+def add_new_tag():
+    """
+    Add a new tag to the system.
+
+    Returns:
+        A JSON object representing the tag added to the system,
+        if addition was successful; aborts with a 400 error
+        otherwise.
+    """
+    tag = WEPTag(request.form["name"], request.form["summary"])
     try:
-        db.session.add(cat)
+        db.session.add(tag)
         db.session.commit()
-        return redirect(url_for("home.show_homepage"))
+        return jsonify(tag.json_serialize())
     except SQLAlchemyError as ex:
         abort(400, ex)
